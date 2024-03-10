09b2676b08841537db84426a7bbd40c2cadf556c
HargunS7 <122636687+HargunS7@users.noreply.github.com>
2024-03-10 14:18:16 +0530

    Update toggle.js

---
+++ toggle.js
@@ -0,0 +1,9 @@
+document.querySelector(".hamburger").addEventListener("click", myFunction);
+
+function myFunction() {
+    document.querySelector(".hamburger").classList.toggle("change")
+
+    document.querySelector(".hamnames").classList.toggle("category")
+
+}
+
